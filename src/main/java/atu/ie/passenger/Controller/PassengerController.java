eb4990f275bf8e9e49646d199d966c929ed9280e
matthew <g00430349@atu.ie>
2025-10-14 23:06:58 +0100

    added passenger controler,service,passenger

--- src/main/java/atu/ie/passenger/Controller/PassengerController.java
+++ src/main/java/atu/ie/passenger/Controller/PassengerController.java
@@ -1,4 +1,43 @@
 package atu.ie.passenger.Controller;
 
+import atu.ie.passenger.model.Passenger;
+import atu.ie.passenger.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/passenger")
 public class PassengerController {
+    private final PassengerService service;
+
+    public PassengerController(PassengerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Passenger>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Passenger> getOne(@PathVariable String id) {
+        Optional<Passenger> maybe = service.findById(id);
+        if (maybe.isPresent()) {
+            return ResponseEntity.ok(maybe.get());
+        } else {return ResponseEntity.notFound().build();
+    }
+}
+@PostMapping
+    public ResponseEntity<Passenger> create(@Valid @RequestBody Passenger p) {
+        Passenger created = service.create(p);
+        return ResponseEntity
+                .created(URI.create("/api/passenger/"+ created.getPassengerId()))
+                .body(created);
+}
 }
--- src/main/java/atu/ie/passenger/model/Passenger.java
+++ src/main/java/atu/ie/passenger/model/Passenger.java
@@ -1,4 +1,16 @@
 package atu.ie.passenger.model;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
+
+@Data @NoArgsConstructor @AllArgsConstructor@Builder
 public class Passenger {
+    @NotBlank @Size( max = 40)
+    private String PassengerId;
+    @NotBlank @Size( max = 60)
+    private String Name;
+    @Email @NotBlank
+    private String Email;
 }
